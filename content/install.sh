e97d097260c833c6027ab36fb2716b1b16184b75
swcoffee <swcoffee@qq.com>
2024-11-25 20:48:45 +0800

    test

--- content/install.sh
+++ content/install.sh
@@ -17,12 +17,12 @@ chmod +x /usr/bin/warp-reg
 /usr/bin/warp-reg > /etc/warp.conf
 # rm /usr/bin/warp-reg
 
-WG_PRIVATE_KEY=$(grep private_key /etc/warp.conf | sed "s|.*: ||")
-WG_PEER_PUBLIC_KEY=$(grep public_key /etc/warp.conf | sed "s|.*: ||")
-WG_IP6_ADDR=$(grep v6 /etc/warp.conf | sed "s|.*: ||")
-WG_RESERVED=$(grep reserved /etc/warp.conf | sed "s|.*: ||")
-if [[ ! "${WG_RESERVED}" =~ , ]]; then
-    WG_RESERVED=\"${WG_RESERVED}\"
-fi
+# WG_PRIVATE_KEY=$(grep private_key /etc/warp.conf | sed "s|.*: ||")
+# WG_PEER_PUBLIC_KEY=$(grep public_key /etc/warp.conf | sed "s|.*: ||")
+# WG_IP6_ADDR=$(grep v6 /etc/warp.conf | sed "s|.*: ||")
+# WG_RESERVED=$(grep reserved /etc/warp.conf | sed "s|.*: ||")
+# if [[ ! "${WG_RESERVED}" =~ , ]]; then
+#     WG_RESERVED=\"${WG_RESERVED}\"
+# fi
 
-sed -i "s|WG_PRIVATE_KEY|${WG_PRIVATE_KEY}|;s|WG_PEER_PUBLIC_KEY|${WG_PEER_PUBLIC_KEY}|;s|fd00::1|${WG_IP6_ADDR}|;s|\[0, 0, 0\]|${WG_RESERVED}|" /workdir/config-wg.json
+# sed -i "s|WG_PRIVATE_KEY|${WG_PRIVATE_KEY}|;s|WG_PEER_PUBLIC_KEY|${WG_PEER_PUBLIC_KEY}|;s|fd00::1|${WG_IP6_ADDR}|;s|\[0, 0, 0\]|${WG_RESERVED}|" /workdir/config-wg.json
